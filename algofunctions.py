5a64a353d6bdb8b71d6068d8a2ed6c411ca18fec
lucas-k-doye <lucas.doye@gmail.com>
2022-12-01 12:40:12 +0000

    Last commit was wrong this is completing my binary search function

--- algofunctions.py
+++ algofunctions.py
@@ -1,6 +1,22 @@
 
 
 
-def binary_search(sorted_list, item)
+def binary_search(sorted_list, item):
     low = 0
     high = len(sorted_list)-1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = sorted_list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            low = mid + 1
+        elif guess < item:
+            high = mid - 1
+        else:
+            return none
+
+
+
+
